fdbff9dd2f97ed440639b893f8af1dc5fb8406fd
kuhamaven <khalil.stessens@ing.austral.edu.ar>
2021-04-13 14:41:26 -0300

    Splitted PSLexer.identifyTokens in smaller methods. Token now has toString() for debugging purposes.

--- lexer/src/main/java/lexer/Optional.java
+++ lexer/src/main/java/lexer/Optional.java
@@ -1,6 +1,40 @@
 package lexer;
 
-public class Optional<T> extends java.util.Optional<T> {
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class Optional<T>{
     private T value;
+
+    private Optional(T value){
+        this.value = value;
+    }
+
+    public static <T> Optional<T> of(T value) {
+        return new Optional<>(Objects.requireNonNull(value));
+    }
+
+    public static <T> Optional<T> empty() {
+        return new Optional<>(null);
+    }
+
+    public T get() {
+        if (value == null) {
+            throw new NoSuchElementException("No value present");
+        }
+        return value;
+    }
+
+    public boolean isPresent() {
+        return value != null;
+    }
+
+    public boolean isEmpty() {
+        return value == null;
+    }
+
+    public void set(T value){
+        this.value = value;
+    }
 
 }
