91ce376b619b18fa1396fae0d19176ff267b08a4
sofiasdz <sofia.sidanez@ing.austral.edu.ar>
2021-04-21 11:01:04 -0300

    code formating test

--- ast_node/src/main/java/ASTNode/testclass.java
+++ ast_node/src/main/java/ASTNode/testclass.java
@@ -1,14 +1,26 @@
 package ASTNode;
 
 public class testclass {
-    int test() {
-        if (true) return 1;
-                                         return 0;
-    }
+  int test() {
+    if (true)
+      return 1;
+    return 0;
+  }
 
-int test2() {
-return 4;
-    }
+  int test2() {
+    return 4;
+  }
 
-int test3() { if (false) return 0;else return 1;}
+  int test3() {
+    if (false)
+      return 0;
+    else
+      return 1;
+  }
+
+  int test5() {
+    if (true)
+      return 9;
+    return 10;
+  }
 }
