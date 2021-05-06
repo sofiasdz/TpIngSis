91b42540801ed89faeaa9a336dc2eb38d7c17c06
kuhamaven <khalil.stessens@ing.austral.edu.ar>
2021-05-05 22:20:51 -0300

    Fixed Syntactic 1.1 error messages

--- SyntacticAnalyzer/src/main/java/syntactic_analyzer/PS11SyntacticAnalyzer.java
+++ SyntacticAnalyzer/src/main/java/syntactic_analyzer/PS11SyntacticAnalyzer.java
@@ -29,19 +29,20 @@ public class PS11SyntacticAnalyzer implements SyntacticAnalyzer {
     if (tokens.get(tokens.size() - 1).getType().equals(TokenType.SEMICOLON)) {
       tokens.remove(tokens.size() - 1);
     } else {
-      throw new RuntimeException("; expected at line: " + tokens.get(0).getStartingLine());
+      throw new RuntimeException(
+              "Error at line " + tokens.get(0).getStartingLine() + ": missing ;");
     }
     if (tokens.get(0).getType().equals(TokenType.LET)) {
       Optional<ASTNode> identifier = ASTNodeIdentifier(tokens.get(1));
       Optional<ASTNode> type = ASTNodeIdentifier(tokens.get(3));
       if (identifier.isEmpty() || type.isEmpty())
         throw new RuntimeException(
-            "Invalid variable declaration at line: " + tokens.get(0).getStartingLine());
+                "Error at line " + tokens.get(0).getStartingLine() + ": Invalid variable declaration");
       Optional<ASTNode> declaration =
-          ASTNodeIdentifier(tokens.get(2), type.get(), identifier.get());
+              ASTNodeIdentifier(tokens.get(2), type.get(), identifier.get());
       if (declaration.isEmpty())
         throw new RuntimeException(
-            "Invalid variable declaration at line: " + tokens.get(0).getStartingLine());
+                "Error at line " + tokens.get(0).getStartingLine() + ": Invalid variable declaration");
       if (tokens.size() < 5) return declaration.get();
       Token assignationToken = tokens.get(4);
       tokens.subList(0, 5).clear();
@@ -51,7 +52,7 @@ public class PS11SyntacticAnalyzer implements SyntacticAnalyzer {
       Optional<ASTNode> identifier = ASTNodeIdentifier(tokens.get(0));
       if (identifier.isEmpty())
         throw new RuntimeException(
-            "Invalid variable declaration at line: " + tokens.get(0).getStartingLine());
+                "Error at line " + tokens.get(0).getStartingLine() + ": Invalid variable declaration");
       Token assignationToken = tokens.get(1);
       tokens.subList(0, 2).clear();
       ASTNode result = operationResolver(tokens);
@@ -60,11 +61,11 @@ public class PS11SyntacticAnalyzer implements SyntacticAnalyzer {
       Optional<ASTNode> print = ASTNodeIdentifier(tokens.get(0));
       if (print.isEmpty())
         throw new RuntimeException(
-            "Invalid printLn declaration at line: " + tokens.get(0).getStartingLine());
+                "Error at line " + tokens.get(0).getStartingLine() + ": Invalid print declaration");
       return print.get();
     } else {
       throw new RuntimeException(
-          "Invalid line start at line: " + tokens.get(0).getStartingLine() + " !");
+              "Error at line " + tokens.get(0).getStartingLine() + ": Invalid line start");
     }
   }
 
